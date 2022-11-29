c1a3fed36573d241163c5b17e0dbe334e352484c
Antonio Navarro <antonio.navarro@iesnervion.es>
2022-11-29 10:56:33 +0100

    Variable final sacada fuera del main

--- src/ejercicios/Ejercicio7E.java
+++ src/ejercicios/Ejercicio7E.java
@@ -1,11 +1,11 @@
 package ejercicios;
 
 public class Ejercicio7E {
-    public static void main(String[] args) {
-        // ^ Definimos la variable num
-        int num = 100000000;
+    // ^ Definimos la variable num
+    static final int NUM = 1000000;
 
+    public static void main(String[] args) {
         // ! Imprimimos el mensaje y dentro llamamos a la función divisoresPrimos]#
-        System.out.println("El número " + num + " tiene " + Methods.divisoresPrimos(num) + " divisores primos.");
+        System.out.println("El número " + Ejercicio7E.NUM + " tiene " + Methods.divisoresPrimos(Ejercicio7E.NUM) + " divisores primos.");
     }
 }
